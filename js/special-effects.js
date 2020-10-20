2def2f5fdf54e024f3c9830108f8849c366cef1c
more8591 <vmoreno8591@gmail.com>
2020-10-20 15:57:34 -0400

    Splash Screen

---
+++ js/special-effects.js
@@ -0,0 +1,15 @@
+//  FILE: Website Effects
+//  Created by Victor Moreno
+//  Date: 10/20/2020
+
+function splash() {
+ $('#splashscreen').function () {
+  $(this).fadeIn(3500);
+  $(this).text("Let me know a bit about you first...").fadeIn(3500);
+ }
+
+$(document).ready(function() {
+
+
+
+});

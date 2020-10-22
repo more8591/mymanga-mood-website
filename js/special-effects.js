11b6067362cff38d8e684d609ef2ec65ea3cc1e5
more8591 <vmoreno8591@gmail.com>
2020-10-22 13:21:18 -0400

    test functions loop

--- js/special-effects.js
+++ js/special-effects.js
@@ -3,10 +3,22 @@
 //  Date: 10/20/2020
 
 function splash() {
- var spl = $('#splashscreen');
+ var loop = function(idx, bundleTXT) {
+  var activeTXT = ".chgTXT" + idx;
+  $(activeTXT)
+   .delay(activeTXT)
+   .fadeIn(fadeDuration)
+   .delay(activeTXT)
+   .fadeOut(fadeDuration, function() {
+    loop( (idx + 1) % bundleTXT, bundleTXT );
+   });
+ }
+ loop(0, $('.chgTXT').length);
 
+ $(bar).fadeIn(3500);
 }
 
 $(document).ready(function() {
-  $(".chgTXT:first").addClass("active");
+
+
 });
